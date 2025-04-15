e519076ee89775d62d2f526114511d0a0a6a9b28
BPKV444\Teilnehmer <paula.staab@web.de>
2025-04-15 11:39:10 +0200

    UPD: Assets/Scenes/Game.unity ADD: BaseCharacter/Controller

--- Assets/script/BaseCharacterController.cs
+++ Assets/script/BaseCharacterController.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
 public class BaseCharacterController : MonoBehaviour
 {
-  public void Movement(CallbackContext ctx )
+    private Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
+
+    /// <summary>
+    /// Movement is called by an input system when the player uses the joystick on the controller 
+    /// </summary>
+    /// <param name="ctx">Context provided by Unity Input</param>
+  public void Movement(CallbackContext ctx)
   {
-        Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
+        //movementInput is set by unity events 
+        movementInput = ctx.ReadValue<Vector2>(); //comment
   }
+  
+  // This is a update 
+    private void Update()
+    {
+        transform.position += new Vector3(movementInput.x /*X axis of Input*/, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+    }
 }
